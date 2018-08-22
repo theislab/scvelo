b5a9a3689065c7dd328f4f27dc55e2e81227f22b
VBergen <volker.bergen@tum.de>
2018-08-22 23:43:32 +0200

    fix tl.transition_matrix

--- scvelo/tools/__init__.py
+++ scvelo/tools/__init__.py
@@ -1,4 +1,4 @@
 from .velocity import velocity
 from .velocity_graph import velocity_graph
-from .velocity_embedding import velocity_embedding
+from .velocity_embedding import transition_matrix, velocity_embedding
 from .velocity_score import score_smoothness, score_transition, score_robustness
