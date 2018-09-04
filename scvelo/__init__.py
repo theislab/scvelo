94ade86255a2ced118b6bbedf83934adea3627ad
VBergen <volker.bergen@tum.de>
2018-09-04 18:45:53 +0200

    workaround for get_version.logger

--- scvelo/__init__.py
+++ scvelo/__init__.py
@@ -1,11 +1,11 @@
 """scvelo - stochastic single cell RNA velocity"""
 
+from get_version import get_version
+__version__ = get_version(__file__)
+del get_version
+
 from . import preprocessing as pp
 from . import tools as tl
 from . import plotting as pl
 from . import datasets
 from . import logging
-
-from get_version import get_version
-__version__ = get_version(__file__)
-del get_version
