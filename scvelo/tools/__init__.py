5020b8bf9984ba9483798a968f82f6f86932df1d
VBergen <volker.bergen@tum.de>
2018-09-04 16:04:28 +0200

    merge markov transition_matrix

--- scvelo/tools/__init__.py
+++ scvelo/tools/__init__.py
@@ -1,5 +1,5 @@
 from .velocity import velocity
 from .velocity_graph import velocity_graph
-from .transition_matrix import transition_matrix, transition_matrix_markov
+from .transition_matrix import transition_matrix
 from .velocity_embedding import velocity_embedding
 from .velocity_score import score_smoothness, score_transition, score_robustness
--- scvelo/tools/transition_matrix.py
+++ scvelo/tools/transition_matrix.py
@@ -1,9 +1,10 @@
+from scipy.spatial.distance import pdist, squareform
 from scipy.sparse import csr_matrix
 import numpy as np
 import warnings
 
 
-def transition_matrix(adata, vkey='velocity', scale=10):
+def transition_matrix(adata, vkey='velocity', basis=None, direction="forward", scale=10):
     """Computes transition probabilities by applying Gaussian kernel to cosine similarities x scale
 
     Arguments
@@ -12,6 +13,10 @@ def transition_matrix(adata, vkey='velocity', scale=10):
         Annotated data matrix.
     vkey: `str` (default: `'velocity'`)
         Name of velocity estimates to be used.
+    basis: `str` or `None` (default: `None`)
+        Restrict transition to embedding if specified
+    direction: `'forward'` or `'backward'` (default: `'foward'`)
+        Whether to use the transition matrix to push forward or to pull backward
     scale: `float` (default: 10)
         Scale parameter of gaussian kernel.
 
@@ -25,31 +30,19 @@ def transition_matrix(adata, vkey='velocity', scale=10):
 
     T = np.expm1(adata.uns[vkey + '_graph'] * scale)
 
+    dists = adata.uns['neighbors']['distances']
+    T = .5 * T + .5 * (dists > 0).multiply(T)
+
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         T = T.multiply(csr_matrix(1. / T.sum(1)))
 
-    return T
-
-
-def transition_matrix_markov(adata, direction="forward"):
-    """Prepare a transition probability for Markov process
-    Arguments
-    ---------
-    adata: :class:`~anndata.AnnData`
-        Annotated data matrix.
-    direction: `str` (default: `'forward'`
-        Whether to diffuse forward of backwards
-    Returns
-    -------
-    Returns sparse matrix with Markov transition probabilities.
-    """
-    T = transition_matrix(adata) if direction == 'forward' else transition_matrix(adata).T
-    T.data[T.data < 1e-10] = 0
-
-    dists = adata.uns['neighbors']['distances']
-    T = .5 * T + .5 * (dists > 0).multiply(T)  # + .2 * np.expm1(dists * dists.mean() / 2)
-
-    T = T.multiply(csr_matrix(1. / T.sum(1)))
+    if basis in adata.obsm.keys():
+        if direction == 'backward': T = T.T
+        X_emb = adata.obsm['X_' + basis]
+        dists_emb = (T > 0).multiply(squareform(pdist(X_emb)))
+        kernel = np.expm1(-dists_emb**2 / dists_emb.data.mean()**2 / 2)
+        T = T.multiply(kernel)
+        T = T.multiply(csr_matrix(1. / T.sum(1)))
 
     return T
