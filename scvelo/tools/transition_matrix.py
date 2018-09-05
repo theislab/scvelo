3722105135fbd07968c1b8261996541adfbb4540
Marius Lange <marius.lange@t-online.de>
2018-09-05 17:46:17 +0200

    Added Locality

--- scvelo/tools/transition_matrix.py
+++ scvelo/tools/transition_matrix.py
@@ -4,7 +4,7 @@ import numpy as np
 import warnings
 
 
-def transition_matrix(adata, vkey='velocity', basis=None, direction="forward", scale=10, p = 0.5):
+def transition_matrix(adata, vkey='velocity', basis=None, direction="forward", scale=10, locality = 2):
     """Computes transition probabilities by applying Gaussian kernel to cosine similarities x scale
 
     Arguments
@@ -26,33 +26,43 @@ def transition_matrix(adata, vkey='velocity', basis=None, direction="forward", s
     -------
     Returns sparse matrix with transition probabilities.
     """
+    
+    #%% check inputs
     if vkey+'_graph' not in adata.uns:
         raise ValueError(
             'You need to run `tl.velocity_graph` first to compute cosine correlations.')
         
-    if p< 0 or p>1:
-        raise ValueError(
-            'You need to choose p in [0, 1].')
-        
     if direction not in ['forward', 'backward']:
         raise ValueError(
             'You need to choose a valid direction')
+        
+    if basis != None and 'X_' + str(basis) not in adata.obsm.keys():
+        raise ValueError(
+                'You need to chose a valid basis')
+    #%% contruct the desired transition matrix
 
+    # get the veclocity based transition matrix
     T = np.expm1(adata.uns[vkey + '_graph'] * scale)
 
+    # get the distance based transition matrix
     dists = adata.uns['neighbors']['distances']
-    T = p * T + (1-p) * (dists > 0).multiply(T)
+    
+    # combine the two
+    T = 0.5 * T + .5 * (dists > 0).multiply(T)
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         T = T.multiply(csr_matrix(1. / T.sum(1)))
+        
+    if direction == 'backward': 
+        T = T.T
+        T = T.multiply(csr_matrix(1. / T.sum(1)))
 
     # compute distances in the embedding to smooth the matrix
-    if basis in adata.obsm.keys():
-        if direction == 'backward': T = T.T
+    if 'X_' + str(basis) in adata.obsm.keys():
         X_emb = adata.obsm['X_' + basis]
         dists_emb = (T > 0).multiply(squareform(pdist(X_emb)))
-        kernel = np.expm1(-dists_emb**2 / dists_emb.data.mean()**2 / 2)
+        kernel = np.expm1(-dists_emb**2 / dists_emb.data.mean()**2 / 2 * 1/locality)
         T = T.multiply(kernel)
         
         # must renormalise
