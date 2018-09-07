bbebba3fcba093ccf9c684738734a8a1b60f7892
VBergen <volker.bergen@tum.de>
2018-09-07 15:01:01 +0200

    add modules: normalize and scale

--- scvelo/tools/utils.py
+++ scvelo/tools/utils.py
@@ -1,4 +1,6 @@
+from scipy.sparse import csr_matrix
 import numpy as np
+import warnings
 
 
 def prod_sum_obs(A, B):
@@ -17,3 +19,23 @@ def norm(A):
     """computes the L2-norm along axis 1 (e.g. genes or embedding dimensions) equivalent to np.linalg.norm(A, axis=1)
     """
     return np.sqrt(np.einsum('ij, ij -> i', A, A))
+
+
+def normalize_sparse(X):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        X = X.multiply(csr_matrix(1. / X.sum(1)))
+    return X
+
+
+def normalize_dense(X):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        X = X / X.sum(1)
+    return X
+
+
+def scale(X, min=0, max=1):
+    X = X - X.min() + min
+    X = X / X.max() * max
+    return X
