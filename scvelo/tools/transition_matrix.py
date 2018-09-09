e8f5c3c94ff78416e110b7117eb525dd8f22a5e4
VBergen <volker.bergen@tum.de>
2018-09-09 15:20:48 +0200

    modify name convention

--- scvelo/tools/transition_matrix.py
+++ scvelo/tools/transition_matrix.py
@@ -3,7 +3,7 @@ from .utils import normalize_sparse
 import numpy as np
 
 
-def transition_matrix(adata, vkey='velocity', basis=None, direction="forward", diffuse_prob=0, scale=10):
+def transition_matrix(adata, vkey='velocity', basis=None, backward=False, diffuse_prob=0, scale=10):
     """Computes transition probabilities by applying Gaussian kernel to cosine similarities x scale
 
     Arguments
@@ -14,10 +14,10 @@ def transition_matrix(adata, vkey='velocity', basis=None, direction="forward", d
         Name of velocity estimates to be used.
     basis: `str` or `None` (default: `None`)
         Restrict transition to embedding if specified
-    direction: `'forward'` or `'backward'` (default: `'foward'`)
-        Whether to use the transition matrix to push forward or to pull backward
+    backward: `bool` (default: `False`)
+        Whether to use the transition matrix to push forward (`False`) or to pull backward (`True`)
     diffuse_prob:
-        Fraction to account for diffusion kernel (Brownian motion)
+        Relative weight to be given to diffusion kernel (Brownian motion)
     scale: `float` (default: 10)
         Scale parameter of gaussian kernel.
 
@@ -30,17 +30,20 @@ def transition_matrix(adata, vkey='velocity', basis=None, direction="forward", d
             'You need to run `tl.velocity_graph` first to compute cosine correlations.')
 
     T = np.expm1(adata.uns[vkey + '_graph'] * scale)
+
+    # weight direct neighbors with 1 and indirect (recurse) neighbors with 0.5
     T = .5 * T + .5 * (adata.uns['neighbors']['distances'] > 0).multiply(T)
 
-    if direction == 'backward': T = T.T
+    if backward: T = T.T
     T = normalize_sparse(T)
 
-    if basis is not None and 'X_' + basis in adata.obsm.keys():
+    if 'X_' + str(basis) in adata.obsm.keys():
         dists_emb = (T > 0).multiply(squareform(pdist(adata.obsm['X_' + basis])))
         sigma = dists_emb.mean()
-        kernel_locality = np.expm1(-.5 * dists_emb.multiply(dists_emb) / sigma**2)
-        T = T.multiply(kernel_locality)
-        T = (1-diffuse_prob) * T + diffuse_prob * np.expm1(-.5 * dists_emb.multiply(dists_emb) / (sigma/2)**2)
+        diffusion_kernel = np.expm1(-.5 * dists_emb.multiply(dists_emb) / sigma**2)
+        T = T.multiply(diffusion_kernel)  # combine velocity based kernel with diffusion based kernel
+        if diffuse_prob > 0:  # add another diffusion kernel (Brownian motion - like)
+            T = (1-diffuse_prob) * T + diffuse_prob * np.expm1(-.5 * dists_emb.multiply(dists_emb) / (sigma/2)**2)
         T = normalize_sparse(T)
 
     return T
