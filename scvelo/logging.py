9c3b05d5f0d0c335615117fce92e264751841843
VBergen <volker.bergen@tum.de>
2018-09-11 10:51:21 +0200

    /

--- scvelo/logging.py
+++ scvelo/logging.py
@@ -4,8 +4,6 @@ from scanpy import logging as logg
 from datetime import datetime
 from time import time
 
-from anndata.logging import print_memory_usage
-
 
 def get_passed_time():
     now = time()
@@ -18,10 +16,15 @@ def print_version():
     from . import __version__
     logg._write_log('Running scvelo', __version__, 'on {}.'.format(datetime.now().strftime("%Y-%m-%d %H:%M")))
 
-    print('Dependencies:', end='  ')
-    for mod in ['scanpy', 'anndata', 'get_version', 'setuptools', 'numpy']:
+
+def print_versions():
+    for mod in ['scvelo', 'scanpy', 'anndata', 'setuptools', 'numpy', 'matplotlib']:
         mod_name = mod[0] if isinstance(mod, tuple) else mod
         mod_install = mod[1] if isinstance(mod, tuple) else mod
         try: print('{}=={}'.format(mod_install, __import__(mod_name).__version__), end='  ')
         except (ImportError, AttributeError): pass
     print()
+
+
+from anndata.logging import print_memory_usage
+print_version_and_date = print_version
