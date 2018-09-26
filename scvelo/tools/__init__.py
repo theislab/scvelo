f06c8d3653f8baa44d68c28253c34368c12822eb
VBergen <volker.bergen@tum.de>
2018-09-26 17:37:55 +0200

    add velocity_confidence, velocity_confidence_transition

--- scvelo/tools/__init__.py
+++ scvelo/tools/__init__.py
@@ -2,5 +2,5 @@ from .velocity import velocity
 from .velocity_graph import velocity_graph
 from .transition_matrix import transition_matrix
 from .velocity_embedding import velocity_embedding
-from .velocity_score import score_smoothness, score_transition, score_robustness
+from .velocity_confidence import velocity_confidence, velocity_confidence_transition
 from .terminal_states import eigs, terminal_states
--- scvelo/tools/velocity_confidence.py
+++ scvelo/tools/velocity_confidence.py
@@ -1,5 +1,5 @@
 from ..logging import logg
-from .utils import prod_sum_var, norm
+from .utils import prod_sum_var, norm, get_indices
 from ..preprocessing.moments import moments
 from .transition_matrix import transition_matrix
 
@@ -7,36 +7,14 @@ from scanpy.api.pp import neighbors
 import numpy as np
 
 
-def score_transition(adata, vkey='velocity', scale=10, copy=False):
-    # compute velocity score as in correlation of mean transition with velocity
+def velocity_confidence(adata, vkey='velocity', copy=False):
     if vkey not in adata.layers.keys():
         raise ValueError(
             'You need to run `tl.velocity` first.')
 
-    dX = transition_matrix(adata, vkey=vkey, scale=scale).dot(adata.layers['Ms']) - adata.layers['Ms']
-    dX -= dX.mean(1)[:, None]
-
-    V = adata.layers[vkey].copy()
-    V -= V.mean(1)[:, None]
-
-    adata.obs[vkey+'_score_transition'] = prod_sum_var(dX, V) / (norm(dX) * norm(V))
-
-    logg.hint(
-        'added to `.obs`\n'
-        '    ' + vkey + '_score_transition')
-
-    return adata if copy else None
-
-
-def score_smoothness(adata, vkey='velocity', copy=False):
-    if vkey not in adata.layers.keys():
-        raise ValueError(
-            'You need to run `tl.velocity` first.')
-
-    X, V = adata.layers['Ms'].copy(), adata.layers[vkey].copy()
-
-    n_neighbors = adata.uns['neighbors']['params']['n_neighbors'] - 1
-    indices = adata.uns['neighbors']['distances'].indices.reshape((-1, n_neighbors))
+    idx = adata.var['velocity_genes']
+    X, V = adata.layers['Ms'][:, idx].copy(), adata.layers[vkey][:, idx].copy()
+    indices = get_indices(dist=adata.uns['neighbors']['distances'])[0]
 
     V -= V.mean(1)[:, None]
     V_norm = norm(V)
@@ -47,11 +25,34 @@ def score_smoothness(adata, vkey='velocity', copy=False):
         Vi_neighs -= Vi_neighs.mean(1)[:, None]
         R[i] = np.mean(np.einsum('ij, j', Vi_neighs, V[i]) / (norm(Vi_neighs) * V_norm[i])[None, :])
 
-    adata.obs[vkey + '_score_smoothness'] = R
+    adata.obs[vkey + '_length'] = V_norm
+    adata.obs[vkey + '_confidence'] = R
 
     logg.hint(
         'added to `.obs`\n'
-        '    ' + vkey + '_score_smoothness')
+        '    ' + vkey + '_confidence')
+
+    return adata if copy else None
+
+
+def velocity_confidence_transition(adata, vkey='velocity', scale=10, copy=False):
+    if vkey not in adata.layers.keys():
+        raise ValueError(
+            'You need to run `tl.velocity` first.')
+
+    idx = adata.var['velocity_genes']
+    T = transition_matrix(adata, vkey=vkey, scale=scale)
+    dX = T.dot(adata.layers['Ms'][:, idx]) - adata.layers['Ms'][:, idx]
+    dX -= dX.mean(1)[:, None]
+
+    V = adata.layers[vkey][:, idx].copy()
+    V -= V.mean(1)[:, None]
+
+    adata.obs[vkey + '_confidence_transition'] = prod_sum_var(dX, V) / (norm(dX) * norm(V))
+
+    logg.hint(
+        'added to `.obs`\n'
+        '    ' + vkey + '_confidence_transition')
 
     return adata if copy else None
 
