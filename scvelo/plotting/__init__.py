45c3e1374550bd6c58b8c704bffaaac70bc5437e
VBergen <volker.bergen@tum.de>
2018-10-01 15:43:45 +0200

    add scv.pl.hist

--- scvelo/plotting/__init__.py
+++ scvelo/plotting/__init__.py
@@ -2,3 +2,4 @@ from .scatter import scatter
 from .velocity import velocity
 from .velocity_embedding import velocity_embedding
 from .velocity_embedding_grid import velocity_embedding_grid
+from .utils import hist
