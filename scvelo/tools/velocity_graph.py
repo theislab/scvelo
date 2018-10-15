24747fd1768a3fd2356fbf1f83d2bb95678c09f1
VBergen <volker.bergen@tum.de>
2018-10-15 19:10:44 +0200

    class VelocityGraph

--- scvelo/tools/velocity_graph.py
+++ scvelo/tools/velocity_graph.py
@@ -1,53 +1,76 @@
 from ..logging import logg, settings
-from .utils import norm, cosine, get_indices, get_iterative_indices
+from .utils import cosine_correlation, get_indices, get_iterative_indices
 from .velocity import velocity
+from .rank_velocity_genes import rank_velocity_genes
 from scanpy.api import Neighbors
 from scipy.sparse import coo_matrix
 import numpy as np
-import warnings
 
 
-class Cosines:
-    def __init__(self, X, V, indices, n_recurse_neighbors, sqrt_transform=False):
-        self.X = X.copy()
-        self.indices = indices
-        self.n_recurse_neighbors = n_recurse_neighbors
-        self.sqrt_transform = sqrt_transform
+def vals_to_csr(vals, rows, cols, shape):
+    graph = coo_matrix((vals, (rows, cols)), shape=shape)
+    graph_neg = graph.copy()
 
-        self.V = V.copy()
-        if sqrt_transform: self.V = np.sqrt(np.abs(self.V)) * np.sign(self.V)
-        self.V = self.V - self.V.mean(1)[:, None]
-        self.V_norm = norm(self.V)
+    graph.data = np.clip(graph.data, 0, 1)
+    graph_neg.data = np.clip(graph_neg.data, -1, 0)
 
-    def compute(self, ixs):
-        vals, rows, cols, async_id = [], [], [], []
+    graph.eliminate_zeros()
+    graph_neg.eliminate_zeros()
 
-        if self.sqrt_transform:
-            for i in ixs:
-                knn_ixs = np.unique(get_iterative_indices(self.indices, i, self.n_recurse_neighbors))
-                dX = self.X[knn_ixs] - self.X[i, None]
-                dX = np.sqrt(np.abs(dX)) * np.sign(dX)
+    return graph.tocsr(), graph_neg.tocsr()
 
-                vals.extend(cosine(dX, self.V[i], self.V_norm[i]))
-                rows.extend(np.ones(len(knn_ixs)) * i)
-                cols.extend(knn_ixs)
 
+class VelocityGraph:
+    def __init__(self, adata, xkey='Ms', vkey='velocity', n_neighbors=None, n_recurse_neighbors=None,
+                 n_top_genes=None, sqrt_trafo=False, use_copy=False):
+        subset = rank_velocity_genes(adata, n_genes=n_top_genes, min_r2=.1, min_dispersion=0, min_counts=10) \
+            if (n_top_genes is not None and n_top_genes < adata.var['velocity_genes'].sum()) \
+            else adata.var['velocity_genes'] if 'velocity_genes' in adata.var.keys() \
+            else np.ones(adata.n_vars, dtype=bool)
+
+        self.X = np.array(adata[:, subset].layers[xkey].copy(), dtype=np.float32)
+        self.V = np.array(adata[:, subset].layers[vkey].copy(), dtype=np.float32)
+
+        self.sqrt_trafo = sqrt_trafo
+        if sqrt_trafo: self.V = np.sqrt(np.abs(self.V)) * np.sign(self.V)
+        self.V -= self.V.mean(1)[:, None]
+
+        self.n_recurse_neighbors = 1 if n_neighbors is not None \
+            else 2 if n_recurse_neighbors is None else n_recurse_neighbors
+
+        if n_neighbors is None and 'neighbors' in adata.uns.keys():
+            self.indices = get_indices(dist=adata.uns['neighbors']['distances'])[0]
         else:
-            for i in ixs:
-                knn_ixs = np.unique(get_iterative_indices(self.indices, i, self.n_recurse_neighbors))
-                dX = self.X[knn_ixs] - self.X[i, None]
-                vals.extend(cosine(dX, self.V[i], self.V_norm[i]))
-                rows.extend(np.ones(len(knn_ixs)) * i)
-                cols.extend(knn_ixs)
-        async_id = int(ixs[0] / len(ixs))  # keep track of order
+            keys = [key for key in ['X_pca', 'X_tsne', 'X_umap'] if key in adata.obsm.keys()]
+            neighs = Neighbors(adata)
+            neighs.compute_neighbors(n_neighbors=n_neighbors, use_rep=keys[-1], n_pcs=10)
+            self.indices = get_indices(dist=neighs.distances)[0]
+
+        self.graph = adata.uns[vkey + '_graph'] if vkey + '_graph' in adata.uns.keys() else []
+        self.graph_neg = adata.uns[vkey + '_graph_neg'] if vkey + '_graph_neg' in adata.uns.keys() else []
+
+    def compute_cosines(self):
+        vals, rows, cols, n_obs = [], [], [], self.X.shape[0]
+        for i in range(n_obs):
+            neighs_idx = get_iterative_indices(self.indices, i, self.n_recurse_neighbors)
+            if self.V[i].max() != 0 or self.V[i].min() != 0:
+                dX = self.X[neighs_idx] - self.X[i, None]  # 60% of runtime
+                if self.sqrt_trafo: dX = np.sqrt(np.abs(dX)) * np.sign(dX)
+                val = cosine_correlation(dX, self.V[i])  # 40% of runtime
+            else:
+                val = np.zeros(len(neighs_idx))
+            vals.extend(val)
+            rows.extend(np.ones(len(neighs_idx)) * i)
+            cols.extend(neighs_idx)
 
         vals = np.hstack(vals)
         vals[np.isnan(vals)] = 1e-10  # actually zero; just to store these entries in sparse matrix.
 
-        return [async_id, vals, rows, cols]
+        self.graph, self.graph_neg = vals_to_csr(vals, rows, cols, shape=(n_obs, n_obs))
 
 
-def velocity_graph(adata, vkey='velocity', n_recurse_neighbors=2, n_neighbors=None, sqrt_transform=False, copy=False):
+def velocity_graph(adata, vkey='velocity', n_neighbors=None, n_recurse_neighbors=None, sqrt_transform=False,
+                   n_top_genes=None, copy=False, use_copy=False):
     """Computes a velocity graph based on cosine similarities.
 
     The cosine similarities are computed between velocities and potential cell state transitions.
@@ -77,45 +100,12 @@ def velocity_graph(adata, vkey='velocity', n_recurse_neighbors=2, n_neighbors=No
 
     logg.info('computing velocity graph', r=True)
 
-    if n_neighbors is not None:
-        keys = [key for key in ['X_pca', 'X_tsne', 'X_umap'] if key in adata.obsm.keys()]
-        neighs = Neighbors(adata)
-        neighs.compute_neighbors(n_neighbors=n_neighbors, use_rep=keys[-1], n_pcs=10)
-        indices = get_indices(dist=neighs.distances)[0]
-        n_recurse_neighbors = 1
-    else:
-        indices = get_indices(dist=adata.uns['neighbors']['distances'])[0]
+    vgraph = VelocityGraph(adata, vkey=vkey, n_neighbors=n_neighbors, n_recurse_neighbors=n_recurse_neighbors,
+                           n_top_genes=n_top_genes, sqrt_trafo=sqrt_transform, use_copy=use_copy)
+    vgraph.compute_cosines()
 
-    cos = Cosines(adata.layers['Ms'][:, adata.var['velocity_genes']],
-                  adata.layers[vkey][:, adata.var['velocity_genes']], indices, n_recurse_neighbors, sqrt_transform)
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        if True:
-            _, vals, rows, cols = cos.compute(range(adata.n_obs))  # list(map(cos.compute, range(adata.n_obs)))
-        else:
-            from multiprocessing import Pool
-            pool = Pool(n_jobs)
-            ranges = np.array_split(range(adata.n_obs), n_jobs)
-            result = pool.map_async(cos.compute, ranges).get()  # async will probably destroy order
-            result.sort(key=lambda tup: tup[0])  # restore order
-            vals, rows, cols = [], [], []
-            for r in result:
-                vals.extend(r[1])
-                rows.extend(r[2])
-                cols.extend(r[3])
-
-    graph = coo_matrix((vals, (rows, cols)), shape=(adata.n_obs, adata.n_obs))
-    graph_neg = graph.copy()
-
-    graph.data = np.clip(graph.data, 0, 1)
-    graph_neg.data = np.clip(graph_neg.data, -1, 0)
-
-    graph.eliminate_zeros()
-    graph_neg.eliminate_zeros()
-
-    adata.uns[vkey+'_graph'] = graph.tocsr()
-    adata.uns[vkey+'_graph_neg'] = graph_neg.tocsr()
+    adata.uns[vkey+'_graph'] = vgraph.graph
+    adata.uns[vkey+'_graph_neg'] = vgraph.graph_neg
 
     logg.info('    finished', time=True, end=' ' if settings.verbosity > 2 else '\n')
     logg.hint(
