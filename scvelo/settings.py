a2ff7b12486b2b906a24667b07b1d0e3a731fdd1
VBergen <volker.bergen@tum.de>
2018-10-27 11:55:45 +0200

    mp defaults for comparison

--- scvelo/settings.py
+++ scvelo/settings.py
@@ -80,34 +80,37 @@ from scanpy.plotting.utils import default_palette
 def set_rcParams_scvelo(fontsize=8, color_map=None):
     """Set matplotlib.rcParams to scvelo defaults."""
 
-    # dpi options
+    # dpi options (mpl default: 100, 100)
     rcParams['figure.dpi'] = 100
     rcParams['savefig.dpi'] = 150
 
-    # figure
+    # figure (mpl default: 0.125, 0.96, 0.15, 0.91)
     rcParams['figure.figsize'] = (7, 5)
     rcParams['figure.subplot.left'] = 0.18
     rcParams['figure.subplot.right'] = 0.96
     rcParams['figure.subplot.bottom'] = 0.15
     rcParams['figure.subplot.top'] = 0.91
 
+    # lines (defaults:  1.5, 6, 1)
     rcParams['lines.linewidth'] = 1.5  # the line width of the frame
     rcParams['lines.markersize'] = 6
     rcParams['lines.markeredgewidth'] = 1
 
     # font
-    rcParams['font.sans-serif'] = ['Arial',
-                                   'Helvetica',
-                                   'DejaVu Sans',
-                                   'Bitstream Vera Sans',
-                                   'sans-serif']
-    fontsize = fontsize
-    rcParams['font.size'] = fontsize
-    rcParams['legend.fontsize'] = 0.9 * fontsize
-    rcParams['axes.titlesize'] = fontsize
-    rcParams['axes.labelsize'] = fontsize
+    rcParams['font.sans-serif'] = \
+        ['Arial', 'Helvetica', 'DejaVu Sans',
+         'Bitstream Vera Sans', 'sans-serif']
 
-    # legend
+    fontsize = fontsize
+    labelsize = 0.9 * fontsize
+
+    # fonsizes (mpl default: 10, medium, large, medium)
+    rcParams['font.size'] = fontsize
+    rcParams['legend.fontsize'] = labelsize
+    rcParams['axes.titlesize'] = fontsize
+    rcParams['axes.labelsize'] = labelsize
+
+    # legend (mpl default: 1, 1, 2, 0.8)
     rcParams['legend.numpoints'] = 1
     rcParams['legend.scatterpoints'] = 1
     rcParams['legend.handlelength'] = 0.5
@@ -116,23 +119,25 @@ def set_rcParams_scvelo(fontsize=8, color_map=None):
     # color cycle
     rcParams['axes.prop_cycle'] = default_palette()
 
-    # lines
+    # axes
     rcParams['axes.linewidth'] = 0.8
     rcParams['axes.edgecolor'] = 'black'
     rcParams['axes.facecolor'] = 'white'
 
-    # ticks
+    # ticks (mpl default: k, k, medium, medium)
     rcParams['xtick.color'] = 'k'
     rcParams['ytick.color'] = 'k'
-    rcParams['xtick.labelsize'] = fontsize
-    rcParams['ytick.labelsize'] = fontsize
+    rcParams['xtick.labelsize'] = labelsize
+    rcParams['ytick.labelsize'] = labelsize
 
-    # axes grid
+    # axes grid (mpl default: False, #b0b0b0)
     rcParams['axes.grid'] = False
     rcParams['grid.color'] = '.8'
 
     # color map
     rcParams['image.cmap'] = rcParams['image.cmap'] if color_map is None else color_map
+
+    # frame (mpl default: True)
     global _frameon
     _frameon = False
 
