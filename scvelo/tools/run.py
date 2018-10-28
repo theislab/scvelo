8ca06c0e5abc64b956f6acc087ca704f9267acb6
Marius Lange <marius.lange@t-online.de>
2018-10-28 15:12:58 +0100

    check the shape

--- scvelo/tools/run.py
+++ scvelo/tools/run.py
@@ -30,7 +30,9 @@ def convert_to_adata(vlm, basis=None):
     layers = OrderedDict()
     layers['spliced'] = vlm.S_sz.T if hasattr(vlm, 'S_sz') else vlm.S.T
     layers['unspliced'] = vlm.U_sz.T if hasattr(vlm, 'U_sz') else vlm.U.T
-    layers['ambiguous'] = vlm.A.T
+    if vlm.A.T.shape == layers['spliced'].shape
+	# this won't always work because many of velocyto's filtering functions don't act on the .A layer
+    	layers['ambiguous'] = vlm.A.T
 
     if hasattr(vlm, 'velocity'): layers['velocity'] = vlm.velocity.T
     if hasattr(vlm, 'Sx_sz'): layers['Ms'] = vlm.Sx_sz.T
