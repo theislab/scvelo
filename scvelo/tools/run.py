5f2c4ec85f08ad92d966b6a595436445a55eda67
Marius Lange <marius.lange@t-online.de>
2018-10-28 14:45:10 +0100

    Changed conversion to adata

--- scvelo/tools/run.py
+++ scvelo/tools/run.py
@@ -30,6 +30,7 @@ def convert_to_adata(vlm, basis=None):
     layers = OrderedDict()
     layers['spliced'] = vlm.S_sz.T if hasattr(vlm, 'S_sz') else vlm.S.T
     layers['unspliced'] = vlm.U_sz.T if hasattr(vlm, 'U_sz') else vlm.U.T
+    layers['ambiguous'] = vlm.A.T
 
     if hasattr(vlm, 'velocity'): layers['velocity'] = vlm.velocity.T
     if hasattr(vlm, 'Sx_sz'): layers['Ms'] = vlm.Sx_sz.T
