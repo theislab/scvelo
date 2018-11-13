86b8089a24b4984de8ed4dc6868d58c5b2b6e0d7
VBergen <volker.bergen@tum.de>
2018-11-13 17:39:13 +0100

    include paga (requires scanpy 1.3.3)

--- scvelo/plotting/__init__.py
+++ scvelo/plotting/__init__.py
@@ -3,3 +3,4 @@ from .velocity import velocity
 from .velocity_embedding import velocity_embedding
 from .velocity_embedding_grid import velocity_embedding_grid
 from .utils import hist
+from scanpy.api.pl import paga, paga_compare
