f9c92b4b0991b75adbeb3a3f1dd316837e35590a
VBergen <volker.bergen@tum.de>
2018-11-17 10:11:47 +0100

    add .utils.randomized_velocity

--- scvelo/utils.py
+++ scvelo/utils.py
@@ -2,7 +2,7 @@ from .preprocessing.utils import show_proportions, cleanup, set_initial_size, ge
 from .preprocessing.moments import get_connectivities, second_order_moments
 
 from .tools.utils import prod_sum_obs, prod_sum_var, norm, vector_norm, R_squared, \
-    cosine_correlation, normalize, scale, get_indices, get_iterative_indices, groups_to_bool
+    cosine_correlation, normalize, scale, get_indices, get_iterative_indices, groups_to_bool, randomized_velocity
 from .tools.rank_velocity_genes import get_mean_var
 from .tools.run import convert_to_adata, convert_to_loom
 from .tools.optimization import leastsq_NxN, leastsq_generalized, maximum_likelihood
