27548f44468ede075e8fa6ae3133be5cd28f9ca2
VBergen <volker.bergen@tum.de>
2018-11-18 20:27:27 +0100

    enable pca_transform in velocity_embedding

--- scvelo/tools/velocity_embedding.py
+++ scvelo/tools/velocity_embedding.py
@@ -19,7 +19,7 @@ def quiver_autoscale(X_emb, V_emb):
 
 
 def velocity_embedding(data, basis=None, vkey='velocity', scale=10, self_transitions=True, use_negative_cosines=True,
-                       autoscale=True, retain_scale=False, all_comps=True, copy=False):
+                       autoscale=True, all_comps=True, retain_scale=False, pca_transform=False, copy=False):
     """Computes the single cell velocities in the embedding
 
     Arguments
@@ -42,11 +42,6 @@ def velocity_embedding(data, basis=None, vkey='velocity', scale=10, self_transit
         coordinates of velocity projection on embedding
     """
     adata = data.copy() if copy else data
-    T = transition_matrix(adata, vkey=vkey, scale=scale, self_transitions=self_transitions, use_negative_cosines=use_negative_cosines)
-    T.setdiag(0)
-    T.eliminate_zeros()
-
-    logg.info('computing velocity embedding', r=True)
 
     if basis is None:
         keys = [key for key in ['pca', 'tsne', 'umap'] if 'X_' + key in adata.obsm.keys()]
@@ -56,27 +51,40 @@ def velocity_embedding(data, basis=None, vkey='velocity', scale=10, self_transit
     if 'X_' + basis not in adata.obsm_keys():
         raise ValueError('You need compute the embedding first.')
 
-    X_emb = adata.obsm['X_' + basis] if all_comps else adata.obsm['X_' + basis][:, :2]
-    V_emb = np.zeros(X_emb.shape)
+    logg.info('computing velocity embedding', r=True)
 
-    TA = T.A if adata.n_obs < 8192 else None
-    sparse = False if adata.n_obs < 8192 else True
+    if 'pca' in basis and pca_transform:
+        V = adata.layers['velocity']
+        PCs = adata.varm['PCs'] if all_comps else adata.varm['PCs'][:, :2]
+        X_emb = adata.obsm['X_' + basis]
+        V_emb = (V - V.mean(0)).dot(PCs)
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        for i in range(adata.n_obs):
-            indices = T[i].indices
-            dX = X_emb[indices] - X_emb[i, None]  # shape (n_neighbors, 2)
-            if not retain_scale: dX /= norm(dX)[:, None]
-            dX[np.isnan(dX)] = 0  # zero diff in a steady-state
-            probs = T[i].data if sparse else TA[i, indices]
-            V_emb[i] = probs.dot(dX) - probs.mean() * dX.sum(0)  # probs.sum() / len(indices)
+    else:
+        X_emb = adata.obsm['X_' + basis] if all_comps else adata.obsm['X_' + basis][:, :2]
+        V_emb = np.zeros(X_emb.shape)
 
-    if retain_scale:
-        delta = T.dot(adata.X) - adata.X
-        if issparse(delta): delta = delta.A
-        cos_proj = (adata.layers[vkey] * delta).sum(1) / norm(delta)
-        V_emb *= np.clip(cos_proj[:, None] * 10, 0, 1)
+        T = transition_matrix(adata, vkey=vkey, scale=scale, self_transitions=self_transitions, use_negative_cosines=use_negative_cosines)
+        T.setdiag(0)
+        T.eliminate_zeros()
+
+        TA = T.A if adata.n_obs < 8192 else None
+        sparse = False if adata.n_obs < 8192 else True
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            for i in range(adata.n_obs):
+                indices = T[i].indices
+                dX = X_emb[indices] - X_emb[i, None]  # shape (n_neighbors, 2)
+                if not retain_scale: dX /= norm(dX)[:, None]
+                dX[np.isnan(dX)] = 0  # zero diff in a steady-state
+                probs = T[i].data if sparse else TA[i, indices]
+                V_emb[i] = probs.dot(dX) - probs.mean() * dX.sum(0)  # probs.sum() / len(indices)
+
+        if retain_scale:
+            delta = T.dot(adata.X) - adata.X
+            if issparse(delta): delta = delta.A
+            cos_proj = (adata.layers[vkey] * delta).sum(1) / norm(delta)
+            V_emb *= np.clip(cos_proj[:, None] * 10, 0, 1)
 
     if autoscale: V_emb /= (3 * quiver_autoscale(X_emb, V_emb))
 
