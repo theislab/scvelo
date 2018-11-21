382b84d2ef5c89d590134a606ff2572a48a6b215
VBergen <volker.bergen@tum.de>
2018-11-21 19:37:06 +0100

    allow groups visualization in velocity_embedding

--- scvelo/plotting/velocity_embedding.py
+++ scvelo/plotting/velocity_embedding.py
@@ -1,5 +1,6 @@
 from ..tools.velocity_embedding import velocity_embedding as compute_velocity_embedding
-from .utils import interpret_colorkey, default_basis, default_size, get_components, savefig
+from ..tools.utils import groups_to_bool
+from .utils import interpret_colorkey, default_basis, default_size, get_components, savefig, default_color
 from .scatter import scatter
 from .docs import doc_scatter, doc_params
 
@@ -47,50 +48,26 @@ def velocity_embedding(adata, basis=None, vkey='velocity', density=1, scale=1, X
         if key + '_' + basis not in adata.obsm_keys() and V is None:
             compute_velocity_embedding(adata, basis=basis, vkey=key)
 
-    if len(colors) > 1:
-        figsize = rcParams['figure.figsize'] if figsize is None else figsize
-        for i, gs in enumerate(pl.GridSpec(1, len(colors), pl.figure(None, (figsize[0] * len(colors), figsize[1]), dpi=dpi))):
-            velocity_embedding(adata, basis=basis, vkey=vkey, layer=layer, density=density, scale=scale,
-                               perc=perc, color=colors[i], use_raw=use_raw, sort_order=sort_order, X=X, V=V,
-                               alpha=alpha, groups=groups, components=components, projection=projection, legend_loc=legend_loc,
-                               legend_fontsize=legend_fontsize, legend_fontweight=legend_fontweight,
-                               color_map=color_map, palette=palette, frameon=frameon, right_margin=right_margin,
-                               left_margin=left_margin, size=size, title=title, show=False, figsize=figsize, dpi=dpi,
-                               save=None, ax=pl.subplot(gs), xlabel=xlabel, ylabel=ylabel, colorbar=colorbar,
-                               fontsize=fontsize, **kwargs)
-        if isinstance(save, str): savefig('' if basis is None else basis, dpi=dpi, save=save, show=show)
-        if show: pl.show()
-        else: return ax
+    scatter_kwargs = {"basis": basis, "perc": perc, "use_raw": use_raw, "sort_order": sort_order, "alpha": alpha,
+                      "components": components, "projection": projection, "legend_loc": legend_loc, "groups": groups,
+                      "legend_fontsize": legend_fontsize, "legend_fontweight": legend_fontweight, "palette": palette,
+                      "color_map": color_map, "frameon": frameon, "title": title, "xlabel": xlabel, "ylabel": ylabel,
+                      "right_margin": right_margin, "left_margin": left_margin, "colorbar": colorbar, "dpi": dpi,
+                      "fontsize": fontsize, "show": False, "save": None}
 
-    elif len(layers) > 1:
+    multikey = colors if len(colors) > 1 else layers if len(layers) > 1 else vkeys if len(vkeys) > 1 else None
+    if multikey is not None:
         figsize = rcParams['figure.figsize'] if figsize is None else figsize
-        for i, gs in enumerate(pl.GridSpec(1, len(layers), pl.figure(None, (figsize[0] * len(layers), figsize[1]), dpi=dpi))):
-            velocity_embedding(adata, basis=basis, vkey=vkey, layer=layers[i], density=density, scale=scale,
-                               perc=None, color=color, use_raw=use_raw, sort_order=sort_order, X=X, V=V, alpha=alpha,
-                               groups=groups, components=components, projection=projection, legend_loc=legend_loc,
-                               legend_fontsize=legend_fontsize, legend_fontweight=legend_fontweight,
-                               color_map=color_map, palette=palette, frameon=frameon, right_margin=right_margin,
-                               left_margin=left_margin, size=size, title=title, show=False, figsize=figsize, dpi=dpi,
-                               save=None, ax=pl.subplot(gs), xlabel=xlabel, ylabel=ylabel, colorbar=colorbar,
-                               fontsize=fontsize, **kwargs)
+        for i, gs in enumerate(pl.GridSpec(1, len(multikey), pl.figure(None, (figsize[0]*len(multikey), figsize[1]), dpi=dpi))):
+            velocity_embedding(adata, X=X, V=V, density=density, scale=scale, size=size, ax=pl.subplot(gs),
+                               color=colors[i] if len(colors) > 1 else color,
+                               layer=layers[i] if len(layers) > 1 else layer,
+                               vkey=vkeys[i] if len(vkeys) > 1 else vkey, **scatter_kwargs, **kwargs)
         if isinstance(save, str): savefig('' if basis is None else basis, dpi=dpi, save=save, show=show)
-        if show: pl.show()
-        else: return ax
-
-    elif len(vkeys) > 1:
-        figsize = rcParams['figure.figsize'] if figsize is None else figsize
-        for i, gs in enumerate(pl.GridSpec(1, len(vkeys), pl.figure(None, (figsize[0] * len(vkeys), figsize[1]), dpi=dpi))):
-            velocity_embedding(adata, basis=basis, vkey=vkeys[i], layer=layer, density=density, scale=scale,
-                               perc=None, color=color, use_raw=use_raw, sort_order=sort_order, X=X, V=V, alpha=alpha,
-                               groups=groups, components=components, projection=projection, legend_loc=legend_loc,
-                               legend_fontsize=legend_fontsize, legend_fontweight=legend_fontweight,
-                               color_map=color_map, palette=palette, frameon=frameon, right_margin=right_margin,
-                               left_margin=left_margin, size=size, title=title, show=False, figsize=figsize, dpi=dpi,
-                               save=None, ax=pl.subplot(gs), xlabel=xlabel, ylabel=ylabel, colorbar=colorbar,
-                               fontsize=fontsize, **kwargs)
-        if isinstance(save, str): savefig('' if basis is None else basis, dpi=dpi, save=save, show=show)
-        if show: pl.show()
-        else: return ax
+        if show:
+            pl.show()
+        else:
+            return ax
 
     else:
         if projection == '3d':
@@ -100,29 +77,27 @@ def velocity_embedding(adata, basis=None, vkey='velocity', density=1, scale=1, X
             ax = pl.figure(None, figsize, dpi=dpi).gca() if ax is None else ax
 
         color, layer, vkey = colors[0], layers[0], vkeys[0]
-        ix_choice = np.random.choice(adata.n_obs, size=int(density * adata.n_obs), replace=False)
+        color = default_color(adata) if color is None else color
+        _adata = adata[groups_to_bool(adata, groups, groupby=color)] if groups is not None and color in adata.obs.keys() else adata
+
+        ix_choice = np.random.choice(_adata.n_obs, size=int(density * _adata.n_obs), replace=False)
 
         x, y = None if X is None else X[:, 0], None if X is None else X[:, 1]
-        X = adata.obsm['X_' + basis][:, get_components(components, basis)][ix_choice] if X is None else X[:, :2][ix_choice]
-        V = adata.obsm[vkey + '_' + basis][:, get_components(components, basis)][ix_choice] if V is None else V[:, :2][ix_choice]
+        X = _adata.obsm['X_' + basis][:, get_components(components, basis)][ix_choice] if X is None else X[:, :2][ix_choice]
+        V = _adata.obsm[vkey + '_' + basis][:, get_components(components, basis)][ix_choice] if V is None else V[:, :2][ix_choice]
 
         quiver_kwargs = {"scale": scale, "cmap": color_map, "angles": 'xy', "scale_units": 'xy', "width": .0005,
                          "edgecolors": 'k', "headlength": 12, "headwidth": 10, "headaxislength": 8, "linewidth": .1}
         quiver_kwargs.update(kwargs)
 
-        c = interpret_colorkey(adata, color, layer, perc)
-        c = c[ix_choice] if len(c) == adata.n_obs else c
+        c = interpret_colorkey(_adata, color, layer, perc)
+        c = c[ix_choice] if len(c) == _adata.n_obs else c
 
         if is_color_like(c[0]): pl.quiver(X[:, 0], X[:, 1], V[:, 0], V[:, 1], color=c, zorder=3, **quiver_kwargs)
         else: pl.quiver(X[:, 0], X[:, 1], V[:, 0], V[:, 1], c, zorder=3, **quiver_kwargs)
 
-        size = default_size(adata)/2 if size is None else size
-        ax = scatter(adata, x=x, y=y, basis=basis, layer=layer, color=color, xlabel=xlabel, ylabel=ylabel, color_map=color_map,
-                     perc=perc, size=size, alpha=alpha, fontsize=fontsize, frameon=frameon, title=title, show=False,
-                     colorbar=colorbar, components=components, figsize=figsize, dpi=dpi, save=None, ax=ax,
-                     use_raw=use_raw, sort_order=sort_order, groups=groups, projection=projection,
-                     legend_loc=legend_loc, legend_fontsize=legend_fontsize, legend_fontweight=legend_fontweight,
-                     palette=palette, right_margin=right_margin, left_margin=left_margin)
+        size = default_size(adata) / 2 if size is None else size,
+        ax = scatter(adata, x=x, y=y, layer=layer, color=color, size=size, ax=ax, **scatter_kwargs)
 
         if isinstance(save, str): savefig('' if basis is None else basis, dpi=dpi, save=save, show=show)
         if show: pl.show()
