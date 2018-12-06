3b6a7453ecb955ab7d413d2cd98a27403d3fb206
VBergen <volker.bergen@tum.de>
2018-12-06 14:15:58 +0100

    /

--- scvelo/preprocessing/__init__.py
+++ scvelo/preprocessing/__init__.py
@@ -1,4 +1,4 @@
 from .utils import show_proportions, cleanup, filter_genes, filter_genes_dispersion, \
     normalize_per_cell, normalize_layers, log1p, filter_and_normalize, recipe_velocity
-from .moments import moments
 from .neighbors import pca, neighbors
+from .moments import moments
