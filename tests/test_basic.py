168195035dc1cea44a17966eba68b97d9b021cd0
VBergen <volker.bergen@tum.de>
2019-06-04 15:41:52 +0200

    minor edit tests (custom clusters instead of louvain)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,8 @@
 import scvelo as scv
+import numpy as np
 
 
 def test_einsum():
-    import numpy as np
     from scvelo.tools.utils import prod_sum_obs, prod_sum_var, norm
     Ms, Mu = np.random.rand(5, 4), np.random.rand(5, 4)
     assert np.allclose(prod_sum_obs(Ms, Mu), np.sum(Ms * Mu, 0))
@@ -21,11 +21,12 @@ def test_pipeline():
     scv.tl.velocity_graph(adata)
     scv.tl.velocity_embedding(adata)
 
-    scv.tl.louvain(adata)
-    scv.tl.cell_fate(adata, groupby='louvain')
-    scv.tl.rank_velocity_genes(adata, match_with='louvain')
-    scv.tl.paga(adata, groups='louvain')
-    scv.tl.paga(adata, groups='louvain', use_rna_velocity=True)
+    scv.tl.velocity_confidence(adata)
+
+    adata.obs['clusters'] = np.repeat([0, 1], int(adata.n_obs / 2))
+    scv.tl.rank_velocity_genes(adata, match_with='clusters')
+    scv.tl.paga(adata, groups='clusters')
+    scv.tl.paga(adata, groups='clusters', use_rna_velocity=True)
 
     scv.pl.velocity(adata, adata.var_names[0])
     scv.pl.velocity_graph(adata)
