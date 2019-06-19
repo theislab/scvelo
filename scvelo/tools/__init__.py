e64d540f92e0e33ae3563a16ec1ab135f5d468d1
VBergen <volker.bergen@tum.de>
2019-06-19 18:48:47 +0200

    edit modules in API

--- scvelo/tools/__init__.py
+++ scvelo/tools/__init__.py
@@ -2,8 +2,8 @@ from .velocity import velocity, velocity_genes
 from .velocity_graph import velocity_graph
 from .transition_matrix import transition_matrix
 from .velocity_embedding import velocity_embedding
-from .velocity_confidence import velocity_confidence, velocity_confidence_transition
-from .terminal_states import cell_fate, cell_origin, eigs, terminal_states
+from .velocity_confidence import velocity_confidence, velocity_confidence_transition, score_robustness
+from .terminal_states import eigs, terminal_states
 from .rank_velocity_genes import velocity_clusters, rank_velocity_genes
 from .velocity_pseudotime import velocity_map, velocity_pseudotime
 from scanpy.api.tl import tsne, umap, diffmap, dpt, louvain, paga
--- scvelo/utils.py
+++ scvelo/utils.py
@@ -6,7 +6,6 @@ from .tools.utils import *
 from .tools.rank_velocity_genes import get_mean_var
 from .tools.run import convert_to_adata, convert_to_loom
 from .tools.optimization import leastsq_NxN, leastsq_generalized, maximum_likelihood, get_weight
-from .tools.velocity_confidence import random_subsample
 from .tools.velocity_graph import vals_to_csr
 
 from .plotting.utils import is_categorical, clip, interpret_colorkey
