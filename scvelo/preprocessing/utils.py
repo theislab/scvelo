1179b9cdc6c5f70f6a0d69eb6aefcc756ff1bb70
VBergen <volker.bergen@tum.de>
2019-07-06 01:11:00 +0200

    ufunc bugfix

--- scvelo/preprocessing/utils.py
+++ scvelo/preprocessing/utils.py
@@ -306,6 +306,7 @@ def normalize_per_cell(data, counts_per_cell_after=None, counts_per_cell=None, k
 
     for layer in layers:
         X = adata.X if layer is 'X' else adata.layers[layer]
+        X = X.astype(np.float32)
         if not_yet_normalized(X) or enforce:
             counts = counts_per_cell if counts_per_cell is not None \
                 else get_initial_size(adata, layer) if use_initial_size else get_size(adata, layer)
@@ -313,12 +314,13 @@ def normalize_per_cell(data, counts_per_cell_after=None, counts_per_cell=None, k
                 counts = counts_per_cell_quantile(X, max_proportion_per_cell, counts)
             # equivalent to scanpy.pp.normalize_per_cell(X, counts_per_cell_after, counts)
             counts_after = np.median(counts) if counts_per_cell_after is None else counts_per_cell_after
-            counts /= counts_after + (counts_after == 0)
+
+            counts = counts / (counts_after + (counts_after == 0))
             counts += counts == 0  # to avoid division by zero
             if issparse(X):
                 sparsefuncs.inplace_row_scale(X, 1 / counts)
             else:
-                X /= np.array(counts[:, None])
+                X = X / np.array(counts[:, None])
             modified_layers.append(layer)
 
     adata.obs['n_counts' if key_n_counts is None else key_n_counts] = get_size(adata)
