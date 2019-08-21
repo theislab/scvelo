c2206806c9be763011adad47bd40002d08037b87
VBergen <volker.bergen@tum.de>
2019-08-21 10:57:31 +0200

    add several temporal alignment modes, min_confidence, basal transcription and kernel_width

--- scvelo/tools/dynamical_model.py
+++ scvelo/tools/dynamical_model.py
@@ -19,7 +19,7 @@ class DynamicsRecovery(BaseDynamics):
         super(DynamicsRecovery, self).__init__(adata, gene, **kwargs)
         if load_pars and 'fit_alpha' in adata.var.keys():
             self.load_pars(adata, gene)
-        else:
+        elif self.recoverable:
             self.initialize()
 
     def initialize(self):
@@ -33,11 +33,11 @@ class DynamicsRecovery(BaseDynamics):
         u, u_w = u / scaling, u_w / scaling
 
         # initialize beta and gamma from extreme quantiles of s
-        #print(self.gene)
         weights_s = s_w >= np.percentile(s_w, perc, axis=0)
         weights_u = u_w >= np.percentile(u_w, perc, axis=0)
         beta, gamma = 1, linreg(convolve(u_w, weights_s), convolve(s_w, weights_s))
-
+        # initialize gamma / beta * scaling clipped to adapt faster to extreme ratios
+        gamma = gamma * 1.5 if gamma < .05 / scaling else gamma / 1.5 if gamma > 3 / scaling else gamma
         u_inf, s_inf = u_w[weights_u | weights_s].mean(), s_w[weights_s].mean()
         u0_, s0_ = u_inf, s_inf
         alpha = u_inf * beta  # np.mean([s_inf * gamma, u_inf * beta])  # np.mean([s0_ * gamma, u0_ * beta])
@@ -51,21 +51,22 @@ class DynamicsRecovery(BaseDynamics):
 
         if self.pval_steady < 1e-3:
             u_inf = np.mean([u_inf, self.u_steady])
-            #s_inf = self.s_steady # np.mean([s_inf, self.s_steady])
+            # s_inf = self.s_steady # np.mean([s_inf, self.s_steady])
+
             alpha = gamma * s_inf
             beta = alpha / u_inf
 
-            #weights_u = u_w >= np.percentile(u_w, perc, axis=0)
-            #u0_, s0_ = u_w[weights_u].mean(), s_w[weights_u].mean()
+            # weights_u = u_w >= np.percentile(u_w, perc, axis=0)
+            # u0_, s0_ = u_w[weights_u].mean(), s_w[weights_u].mean()
+
             u0_, s0_ = u_inf, s_inf
 
         # alpha, beta, gamma = np.array([alpha, beta, gamma]) * scaling
         t_ = tau_inv(u0_, s0_, 0, 0, alpha, beta, gamma)
 
         # update object with initialized vars
-        alpha_, u0, s0, = 0, 0, 0
-        self.alpha, self.beta, self.gamma, self.alpha_, self.scaling = alpha, beta, gamma, alpha_, scaling
-        self.u0, self.s0, self.u0_, self.s0_, self.t_ = u0, s0, u0_, s0_, t_
+        self.alpha, self.beta, self.gamma, self.scaling, self.alpha_,  = alpha, beta, gamma, scaling, 0
+        self.u0_, self.s0_, self.t_ = u0_, s0_, t_
         self.pars = np.array([alpha, beta, gamma, self.t_, self.scaling])[:, None]
 
         # initialize time point assignment
@@ -77,21 +78,7 @@ class DynamicsRecovery(BaseDynamics):
 
         self.steady_state_ratio = self.gamma / self.beta
 
-        if self.high_pars_resolution:
-            self.fit_t_and_alpha()
-            self.fit_scaling_()
-            self.fit_rates()
-            self.fit_t_()
-            self.fit_t_and_rates()
-
-        # Overwrite callbacks
-        if not self.high_pars_resolution:
-            self.cb_fit_t_and_alpha = None
-            self.cb_fit_scaling_ = None
-            self.cb_fit_rates = None
-            self.cb_fit_t_ = None
-            self.cb_fit_t_and_rates = None
-            self.cb_fit_rates_all = None
+        self.set_callbacks()
 
     def initialize_scaling(self, sight=.5):  # fit scaling and update if improved
         z_vals = self.scaling + np.linspace(-1, 1, num=5) * self.scaling * sight
@@ -102,7 +89,6 @@ class DynamicsRecovery(BaseDynamics):
 
     def fit(self, assignment_mode=None):
         if self.max_iter > 0:
-
             # pre-train with explicit time assignment
             self.fit_t_and_alpha()
             self.fit_scaling_()
@@ -180,6 +166,16 @@ class DynamicsRecovery(BaseDynamics):
     def cb_fit_rates_all(self, x):
         self.update(alpha=x[0], beta=x[1], gamma=x[2])
 
+    def set_callbacks(self):
+        # Overwrite callbacks
+        if not self.high_pars_resolution:
+            self.cb_fit_t_and_alpha = None
+            self.cb_fit_scaling_ = None
+            self.cb_fit_rates = None
+            self.cb_fit_t_ = None
+            self.cb_fit_t_and_rates = None
+            self.cb_fit_rates_all = None
+
     def update(self, t=None, t_=None, alpha=None, beta=None, gamma=None, scaling=None, u0_=None, s0_=None, adjust_t_=True):
         loss_prev = self.loss[-1] if len(self.loss) > 0 else 1e6
 
@@ -225,23 +221,27 @@ class DynamicsRecovery(BaseDynamics):
         return perform_update
 
 
-def read_pars(adata, pars_names=['alpha', 'beta', 'gamma', 't_', 'scaling', 'std_u', 'std_s', 'likelihood'], key='fit'):
+default_pars_names = ['alpha', 'beta', 'gamma', 't_', 'scaling', 'std_u', 'std_s', 'likelihood', 'u0', 's0']
+
+
+def read_pars(adata, pars_names=None, key='fit'):
     pars = []
-    for name in pars_names:
+    for name in (default_pars_names if pars_names is None else pars_names):
         pkey = key + '_' + name
         par = adata.var[pkey].values if pkey in adata.var.keys() else np.zeros(adata.n_vars) * np.nan
         pars.append(par)
     return pars
 
 
-def write_pars(adata, pars, pars_names=['alpha', 'beta', 'gamma', 't_', 'scaling', 'std_u', 'std_s', 'likelihood'], add_key='fit'):
-    for i, name in enumerate(pars_names):
+def write_pars(adata, pars, pars_names=None, add_key='fit'):
+    for i, name in enumerate(default_pars_names if pars_names is None else pars_names):
         adata.var[add_key + '_' + name] = pars[i]
 
 
 def recover_dynamics(data, var_names='velocity_genes', max_iter=10, assignment_mode='projection', t_max=None,
-                     fit_scaling=True, fit_time=True, fit_steady_states=True, fit_connected_states=True, use_raw=False,
-                     load_pars=None, return_model=True, plot_results=False, add_key='fit', copy=False, **kwargs):
+                     fit_scaling=True, fit_time=True, fit_steady_states=True, fit_connected_states=None,
+                     fit_basal_transcription=None, use_raw=False, load_pars=None, return_model=True, plot_results=False,
+                     add_key='fit', copy=False, **kwargs):
     """Estimates velocities in a gene-specific manner
 
     Arguments
@@ -256,18 +256,31 @@ def recover_dynamics(data, var_names='velocity_genes', max_iter=10, assignment_m
     adata = data.copy() if copy else data
     logg.info('recovering dynamics', r=True)
 
+    if 'Ms' not in adata.layers.keys() or 'Mu' not in adata.layers.keys(): use_raw = True
+    if fit_connected_states is None: fit_connected_states = not use_raw
+
+    adata.uns['recover_dynamics'] = {'fit_connected_states': fit_connected_states,
+                                     'fit_basal_transcription': fit_basal_transcription, 'use_raw': use_raw}
+
     if isinstance(var_names, str) and var_names not in adata.var_names:
-        var_names = adata.var_names[adata.var[var_names] == True] if 'genes' in var_names and var_names in adata.var.keys() \
-            else adata.var_names if 'all' in var_names or 'genes' in var_names else var_names
-    var_names = make_unique_list(var_names, allow_array=True)
-    var_names = [name for name in var_names if name in adata.var_names]
+        if var_names in adata.var.keys():
+            var_names = adata.var_names[adata.var[var_names].values]
+        elif '_genes' in var_names:
+            from .velocity import Velocity
+            velo = Velocity(adata, use_raw=use_raw)
+            velo.compute_deterministic(perc=[5, 95])
+            var_names = adata.var_names[velo._velocity_genes]
+        elif var_names is 'all':
+            var_names = adata.var_names
+        else:
+            raise ValueError('Variable name not found in var keys.')
+
+    var_names = [name for name in make_unique_list(var_names, allow_array=True) if name in adata.var_names]
     if len(var_names) == 0:
         raise ValueError('Variable name not found in var keys.')
 
-    if fit_connected_states is True:
-        fit_connected_states = get_connectivities(adata)
-
-    alpha, beta, gamma, t_, scaling, std_u, std_s, likelihood = read_pars(adata)
+    alpha, beta, gamma, t_, scaling, std_u, std_s, likelihood, u0, s0 = read_pars(adata)
+    likelihood[np.isnan(likelihood)] = 0
     idx, L, P = [], [], []
     T = adata.layers['fit_t'] if 'fit_t' in adata.layers.keys() else np.zeros(adata.shape) * np.nan
     Tau = adata.layers['fit_tau'] if 'fit_tau' in adata.layers.keys() else np.zeros(adata.shape) * np.nan
@@ -276,40 +289,47 @@ def recover_dynamics(data, var_names='velocity_genes', max_iter=10, assignment_m
     progress = logg.ProgressReporter(len(var_names))
     for i, gene in enumerate(var_names):
         dm = DynamicsRecovery(adata, gene, use_raw=use_raw, load_pars=load_pars, max_iter=max_iter, fit_time=fit_time,
-                              fit_scaling=fit_scaling, fit_steady_states=fit_steady_states,
-                              fit_connected_states=fit_connected_states, **kwargs)
-        dm.fit(assignment_mode=assignment_mode)
+                              fit_steady_states=fit_steady_states,
+                              fit_connected_states=get_connectivities(adata) if fit_connected_states else fit_connected_states,
+                              fit_scaling=fit_scaling, fit_basal_transcription=fit_basal_transcription, **kwargs)
+        if dm.recoverable:
+            dm.fit(assignment_mode=assignment_mode)
 
-        ix = np.where(adata.var_names == gene)[0][0]
-        idx.append(ix)
+            ix = np.where(adata.var_names == gene)[0][0]
+            idx.append(ix)
 
-        T[:, ix], Tau[:, ix], Tau_[:, ix] = dm.t, dm.tau, dm.tau_
-        alpha[ix], beta[ix], gamma[ix], t_[ix], scaling[ix] = dm.pars[:, -1]
-        std_u[ix], std_s[ix], likelihood[ix] = dm.std_u, dm.std_s, dm.likelihood
-        L.append(dm.loss)
-        if plot_results and i < 4:
-            P.append(np.array(dm.pars))
+            T[:, ix], Tau[:, ix], Tau_[:, ix] = dm.t, dm.tau, dm.tau_
+            alpha[ix], beta[ix], gamma[ix], t_[ix], scaling[ix] = dm.pars[:, -1]
+            u0[ix], s0[ix] = dm.u0, dm.s0
+            std_u[ix], std_s[ix], likelihood[ix] = dm.std_u, dm.std_s, dm.likelihood
+            L.append(dm.loss)
+            if plot_results and i < 4:
+                P.append(np.array(dm.pars))
 
-        progress.update()
+            progress.update()
+        else:
+            logg.warn(dm.gene, 'not recoverable due to insufficient samples.')
+            dm = None
     progress.finish()
 
-    write_pars(adata, [alpha, beta, gamma, t_, scaling, std_u, std_s, likelihood])
+    write_pars(adata, [alpha, beta, gamma, t_, scaling, std_u, std_s, likelihood, u0, s0])
     adata.layers['fit_t'] = T
     adata.layers['fit_tau'] = Tau
     adata.layers['fit_tau_'] = Tau_
 
-    cur_len = adata.varm['loss'].shape[1] if 'loss' in adata.varm.keys() else 2
-    max_len = max(np.max([len(l) for l in L]), cur_len)
-    loss = np.ones((adata.n_vars, max_len)) * np.nan
+    if L:  # is False if only one invalid / irrecoverable gene was given in var_names
+        cur_len = adata.varm['loss'].shape[1] if 'loss' in adata.varm.keys() else 2
+        max_len = max(np.max([len(l) for l in L]), cur_len) if L else cur_len
+        loss = np.ones((adata.n_vars, max_len)) * np.nan
 
-    if 'loss' in adata.varm.keys():
-        loss[:, :cur_len] = adata.varm['loss']
+        if 'loss' in adata.varm.keys():
+            loss[:, :cur_len] = adata.varm['loss']
 
-    loss[idx] = np.vstack([np.concatenate([l, np.ones(max_len-len(l)) * np.nan]) for l in L])
-    adata.varm['loss'] = loss
+        loss[idx] = np.vstack([np.concatenate([l, np.ones(max_len-len(l)) * np.nan]) for l in L])
+        adata.varm['loss'] = loss
 
     if t_max is not False:
-        dm, m = align_dynamics(adata, t_max=t_max, dm=dm, idx=idx)
+        dm = align_dynamics(adata, t_max=t_max, dm=dm, idx=idx)
 
     logg.info('    finished', time=True, end=' ' if settings.verbosity > 2 else '\n')
     logg.hint('added \n' 
@@ -322,7 +342,9 @@ def recover_dynamics(data, var_names='velocity_genes', max_iter=10, assignment_m
         fig, axes = pl.subplots(nrows=n_rows, ncols=6, figsize=figsize)
         pl.subplots_adjust(wspace=0.7, hspace=0.5)
         for i, gene in enumerate(var_names[:4]):
-            if t_max is not False: P[i] *= np.array([1 / m[i], 1 / m[i], 1 / m[i], m[i], 1])[:, None]
+            if t_max is not False:
+                mi = dm.m[i]
+                P[i] *= np.array([1 / mi, 1 / mi, 1 / mi, mi, 1])[:, None]
             ax = axes[i] if n_rows > 1 else axes
             for j, pij in enumerate(P[i]):
                 ax[j].plot(pij)
@@ -333,41 +355,78 @@ def recover_dynamics(data, var_names='velocity_genes', max_iter=10, assignment_m
     return dm if return_model else adata if copy else None
 
 
-def align_dynamics(data, t_max=None, dm=None, idx=None, copy=False):
+def align_dynamics(data, t_max=None, dm=None, idx=None, mode=None, remove_outliers=None, copy=False):
     adata = data.copy() if copy else data
-    alpha, beta, gamma, t_, m = read_pars(adata, pars_names=['alpha', 'beta', 'gamma', 't_', 'alignment_scaling'])
+    alpha, beta, gamma, t_, scaling, mz = read_pars(adata, pars_names=['alpha', 'beta', 'gamma', 't_', 'scaling', 'alignment_scaling'])
     T = adata.layers['fit_t'] if 'fit_t' in adata.layers.keys() else np.zeros(adata.shape) * np.nan
     Tau = adata.layers['fit_tau'] if 'fit_tau' in adata.layers.keys() else np.zeros(adata.shape) * np.nan
     Tau_ = adata.layers['fit_tau_'] if 'fit_tau_' in adata.layers.keys() else np.zeros(adata.shape) * np.nan
     idx = ~ np.isnan(np.sum(T, axis=0)) if idx is None else idx
+    if 'fit_alignment_scaling' not in adata.var.keys(): mz = np.ones(adata.n_vars)
+    if mode is None: mode = 'align_increments' if adata.uns['recover_dynamics']['use_raw'] else 'align_total_time'
 
     m = np.ones(adata.n_vars)
-    if t_max is not False:
+    mz_prev = np.array(mz)
+
+    if dm is not None:  # newly fitted
+        mz[idx] = 1
+
+    if mode is 'common_splicing_rate':
+        m[idx] = beta[idx] / scaling[idx]
+        mz *= m
+    elif mode is 'common_scaling':
+        m[idx] = scaling[idx]
+        mz *= m
+    elif mode is 'align_increments' and t_max is not False:
         dt = compute_dt(T[:, idx])
+        n_obs = dt.shape[0]
 
-        dt_min = t_[idx] / dt.shape[0] / 5
-        idx_bool = dt > dt_min
+        idx_bool = dt > t_[idx] / n_obs
         dt *= idx_bool / idx_bool
+        mdt = np.nanmedian(dt, axis=0)
 
-        dt_mean = np.nanmean(dt, axis=0)
-        dt_mean += dt_mean == 0
+        mdt += mdt == 0
 
         t_max = 100 if t_max is None else t_max
-        m[idx] = t_max / (dt_mean * len(dt))
+        m[idx] = t_max / (mdt * n_obs)
+        mz *= m
+
+    elif mode is 'align_total_time' and t_max is not False:
+        T_max = np.max(T[:, idx] * (T[:, idx] < t_[idx]), axis=0) \
+                + np.max((T[:, idx] - t_[idx]) * (T[:, idx] > t_[idx]), axis=0)
+
+        denom = 1 - np.sum((T[:, idx] == t_[idx]) | (T[:, idx] == 0), axis=0) / len(T)
+        denom += denom == 0
+
+        T_max = T_max / denom
+        T_max += T_max == 0
+
+        t_max = 100 if t_max is None else t_max
+        m[idx] = t_max / T_max
+        mz *= m
+
+    else:
+        m = 1 / mz
+        mz = np.ones(adata.n_vars)
+
+    if remove_outliers:
+        mu, std = np.nanmean(mz), np.nanstd(mz)
+        mz = np.clip(mz, mu - 3 * std, mu + 3 * std)
+        m = mz / mz_prev
 
     alpha, beta, gamma, T, t_, Tau, Tau_ = alpha / m, beta / m, gamma / m, T * m, t_ * m, Tau * m, Tau_ * m
 
-    write_pars(adata, [alpha, beta, gamma, t_, m], pars_names=['alpha', 'beta', 'gamma', 't_', 'alignment_scaling'])
+    write_pars(adata, [alpha, beta, gamma, t_, mz], pars_names=['alpha', 'beta', 'gamma', 't_', 'alignment_scaling'])
     adata.layers['fit_t'] = T
     adata.layers['fit_tau'] = Tau
     adata.layers['fit_tau_'] = Tau_
 
-    m = m[idx]
     if dm is not None:
-        dm.alpha, dm.beta, dm.gamma, dm.pars[:3] = np.array([dm.alpha, dm.beta, dm.gamma, dm.pars[:3]]) / m[-1]
-        dm.t, dm.tau, dm.t_, dm.pars[4] = np.array([dm.t, dm.tau, dm.t_, dm.pars[4]]) * m[-1]
+        dm.m = m[idx]
+        dm.alpha, dm.beta, dm.gamma, dm.pars[:3] = np.array([dm.alpha, dm.beta, dm.gamma, dm.pars[:3]]) / dm.m[-1]
+        dm.t, dm.tau, dm.t_, dm.pars[4] = np.array([dm.t, dm.tau, dm.t_, dm.pars[4]]) * dm.m[-1]
 
-    return adata if copy else (dm, m)
+    return adata if copy else dm
 
 
 def recover_latent_time(data, copy=False):
