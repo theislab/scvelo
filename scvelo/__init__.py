780a8e6704ab4e5b36b4c21e0f2dfafab9012af2
VBergen <volker.bergen@tum.de>
2019-09-18 09:59:29 +0200

    versioning via get_version

--- scvelo/__init__.py
+++ scvelo/__init__.py
@@ -1,13 +1,8 @@
 """scvelo - stochastic single cell RNA velocity"""
 
-try:
-    from setuptools_scm import get_version
-    __version__ = get_version(root='..', relative_to=__file__)
-    del get_version
-except:
-    from .get_version import get_version
-    __version__ = get_version(__file__)
-    del get_version
+from .get_version import get_version
+__version__ = get_version(__file__)
+del get_version
 
 from .read_load import AnnData, read, read_loom, load, read_csv
 from .preprocessing.neighbors import Neighbors
