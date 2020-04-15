small bugfix (for color gradient plot)

scvelo/plotting/scatter.py
@@ -143,7 +143,7 @@ def scatter(adata=None, x=None, y=None, basis=None, vkey=None, color=None, use_r
         elif color_gradients is not None and color_gradients is not False:
             vals, names, color, scatter_kwargs = gets_vals_from_color_gradients(adata, color, **scatter_kwargs)
             c_colors = {cat: col for (cat, col) in zip(adata.obs[color].cat.categories, adata.uns[color + '_colors'])}
-            ax = scatter(adata, color='grey', **get_kwargs(scatter_kwargs, {'alpha': 0.05}))  # grey background
+            ax = scatter(adata, color='grey', ax=ax, **get_kwargs(scatter_kwargs, {'alpha': 0.05}))  # grey background
             ax = scatter(adata, color=color, ax=ax, **get_kwargs(scatter_kwargs, {'s': 0}))  # set legend
             sorted_idx = np.argsort(vals, 1)[:, ::-1][:, :2]
             for id0 in range(len(names)):
@@ -208,8 +208,8 @@ def scatter(adata=None, x=None, y=None, basis=None, vkey=None, color=None, use_r
 
                 # velocity model fits (full dynamics and steady-state ratios)
                 if any(['gamma' in key or 'alpha' in key for key in adata.var.keys()]):
-                    plot_velocity_fits(adata, basis, vkey, use_raw, linewidth, linecolor, legend_loc_lines, legend_fontsize,
-                                       add_assignments, ax=ax)
+                    plot_velocity_fits(adata, basis, vkey, use_raw, linewidth, linecolor,
+                                       legend_loc_lines, legend_fontsize, add_assignments, ax=ax)
 
             # embedding: set x and y to embedding coordinates
             elif is_embedding:

scvelo/plotting/utils.py
@@ -349,6 +349,7 @@ def _add_legend(adata, ax, value_to_plot, legend_loc, scatter_array, legend_font
         texts = []
         for ilabel, label in enumerate(categories):
             x_pos, y_pos = np.nanmedian(scatter_array[obs_vals == label, :], axis=0)
+            if isinstance(label, str): label = label.replace('_', ' ')
             text = ax.text(x_pos, y_pos, label, weight=legend_fontweight, verticalalignment='center',
                            horizontalalignment='center', fontsize=legend_fontsize, path_effects=legend_fontoutline)
             texts.append(text)
@@ -359,6 +360,7 @@ def _add_legend(adata, ax, value_to_plot, legend_loc, scatter_array, legend_font
 
     else:
         for idx, label in enumerate(categories):
+            if isinstance(label, str): label = label.replace('_', ' ')
             ax.scatter([], [], c=[colors[idx]], label=label)
         ncol = (1 if len(categories) <= 14 else 2 if len(categories) <= 30 else 3)
         if legend_loc == 'upper right':
@@ -744,8 +746,7 @@ def plot_polyfit(x, y, add_polyfit=True, add_legend=True, color=None, linewidth=
 def plot_vlines(adata, basis, vkey, xkey, linewidth=1, linecolor=None, ax=None):
     if ax is None: ax = pl.gca()
     xnew = np.linspace(0, np.percentile(make_dense(adata[:, basis].layers[xkey]), 98))
-    vkeys = ['velocity'] if vkey is None else make_unique_list(vkey)
-    fits = [fit for fit in vkeys if all(['velocity' in fit, fit + '_gamma' in adata.var.keys()])]
+    fits = [fit for fit in make_unique_list(vkey) if all(['velocity' in fit, fit + '_gamma' in adata.var.keys()])]
     linecolor, lines = to_list(linecolor), []
     for i, fit in enumerate(fits):
         linestyle = '--' if 'variance_' + fit in adata.layers.keys() else '-'
@@ -765,6 +766,7 @@ def plot_velocity_fits(adata, basis, vkey=None, use_raw=None, linewidth=None, li
     if use_raw is None: use_raw = 'Ms' not in adata.layers.keys()
 
     # linear fits
+    if vkey is None: vkey = 'dynamics' if 'fit_alpha' in adata.var.keys() else 'velocity'
     lines, fits = plot_vlines(adata, basis, vkey, 'spliced' if use_raw else 'Ms', linewidth, linecolor, ax=ax)
 
     # full dynamic fits