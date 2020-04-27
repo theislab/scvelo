be11cc22ca0239bc28bab3305c75214b1b6fb4d2
VBergen <volker.bergen@tum.de>
2020-04-28 00:01:14 +0200

    extend PAGA with time_prior and MST

--- scvelo/plotting/paga.py
+++ scvelo/plotting/paga.py
@@ -14,9 +14,9 @@ from scanpy.plotting._tools.paga import paga as scanpy_paga
 
 @doc_params(scatter=doc_scatter)
 def paga(adata, basis=None, vkey='velocity', color=None, layer=None, title=None, threshold=None, layout=None,
-         layout_kwds={}, init_pos=None, root=0, labels=None, single_component=False, solid_edges='connectivities',
-         dashed_edges='connectivities', transitions='transitions_confidence', node_size_scale=1, node_size_power=0.5,
-         edge_width_scale=.4, min_edge_width=None, max_edge_width=1, arrowsize=15, random_state=0, pos=None,
+         layout_kwds={}, init_pos=None, root=0, labels=None, single_component=False, dashed_edges='connectivities',
+         solid_edges='transitions_confidence', transitions='transitions_confidence', node_size_scale=1, node_size_power=0.5,
+         edge_width_scale=.4, min_edge_width=None, max_edge_width=2, arrowsize=15, random_state=0, pos=None,
          node_colors=None, normalize_to_color=False, cmap=None, cax=None, cb_kwds={}, add_pos=True,
          export_to_gexf=False, plot=True, use_raw=None, size=None, groups=None, components=None, figsize=None, dpi=None,
          show=True, save=None, ax=None, ncols=None, scatter_flag=None, **kwargs):
@@ -26,10 +26,6 @@ def paga(adata, basis=None, vkey='velocity', color=None, layer=None, title=None,
     ---------
     adata: :class:`~anndata.AnnData`
         Annotated data matrix.
-    x: `str`, `np.ndarray` or `None` (default: `None`)
-        x coordinate
-    y: `str`, `np.ndarray` or `None` (default: `None`)
-        y coordinate
     vkey: `str` or `None` (default: `None`)
         Key for annotations of observations/cells or variables/genes.
     {scatter}
@@ -44,6 +40,8 @@ def paga(adata, basis=None, vkey='velocity', color=None, layer=None, title=None,
     layers, vkeys, colors = make_unique_list(layer), make_unique_list(vkey), make_unique_list(color, allow_array=True)
     node_colors = colors if node_colors is None else node_colors
     bases = [default_basis(adata) if basis is None else basis for basis in make_unique_valid_list(adata, basis)]
+    if transitions not in adata.uns['paga']: transitions = None
+    if min_edge_width is not None: max_edge_width = max(min_edge_width, max_edge_width)
 
     if threshold is None and 'threshold' in adata.uns['paga']: threshold = adata.uns['paga']['threshold']
     paga_kwargs = {'threshold': threshold, 'layout': layout, 'layout_kwds': layout_kwds, 'init_pos': init_pos,
@@ -92,8 +90,11 @@ def paga(adata, basis=None, vkey='velocity', color=None, layer=None, title=None,
         color, layer, vkey, basis = colors[0], layers[0], vkeys[0], basis
         color = default_color(adata) if color is None else color
         size = default_size(adata) / 2 if size is None else size
-        _adata = adata[
-            groups_to_bool(adata, groups, groupby=color)] if groups is not None and color in adata.obs.keys() else adata
+        groups = groups if isinstance(groups, str) and groups in adata.obs.keys() \
+            else 'clusters' if 'clusters' in adata.obs.keys() \
+            else 'louvain' if 'louvain' in adata.obs.keys() else None
+        _adata = adata[groups_to_bool(adata, groups, groupby=color)] \
+            if groups is not None and color in adata.obs.keys() else adata
 
         if basis in adata.var_names and basis is not None:
             x = adata[:, basis].layers['spliced'] if use_raw else adata[:, basis].layers['Ms']
@@ -120,12 +121,26 @@ def paga(adata, basis=None, vkey='velocity', color=None, layer=None, title=None,
 
         legend_loc = kwargs.pop('legend_loc', None)
         kwargs['legend_loc'] = 'none' if legend_loc == 'on data' else legend_loc  # let paga handle 'on data'
+        if 'frameon' not in paga_kwargs or not paga_kwargs['frameon']:
+            paga_kwargs['frameon'] = False
+        kwargs['frameon'] = paga_kwargs['frameon']
 
         ax = pl.figure(None, figsize, dpi=dpi).gca() if ax is None else ax
         if scatter_flag and basis is not None:
             if 'alpha' not in kwargs: kwargs['alpha'] = .5
             ax = scatter(adata, basis=basis, x=x, y=y, vkey=vkey, layer=layer, color=color, size=size, title=title,
                          ax=ax, save=None, zorder=0, show=False, **kwargs)
+        else:
+            basis = default_basis(adata)
+            if basis is not None and init_pos is None or isinstance(init_pos, str):
+                cats = adata.obs[groups].cat.categories
+                X_emb = adata.obsm['X_' + basis]
+                init_pos = np.stack([np.median(X_emb[adata.obs[groups] == c], axis=0) for c in cats])
+                paga_kwargs['init_pos'] = init_pos
+                kwargs['alpha'] = 0
+                x, y = np.ones(len(X_emb)) * np.mean(init_pos[:, 0]), np.ones(len(X_emb)) * np.mean(init_pos[:, 1])
+                ax = scatter(adata, x=x, y=y, title=title, ax=ax, save=None, zorder=0, show=False, **kwargs)
+
         scanpy_paga(adata, ax=ax, show=False,  **paga_kwargs,
                     text_kwds={'zorder': 1000, 'alpha': legend_loc == 'on data'})
 
--- scvelo/tools/paga.py
+++ scvelo/tools/paga.py
@@ -2,6 +2,8 @@
 from .. import settings
 from .. import logging as logg
 from .utils import strings_to_categoricals, most_common_in_list
+from .velocity_graph import vals_to_csr
+from .velocity_pseudotime import velocity_pseudotime
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
@@ -53,11 +55,16 @@ def get_sparse_from_igraph(graph, weight_attr=None):
         return csr_matrix(shape)
 
 
-class PAGA2(PAGA):
-    def __init__(self, adata, groups, model='v1.2', vkey='velocity'):
-        super().__init__(adata=adata, groups=groups, model=model)
-        self.vkey = vkey
+class PAGA_tree(PAGA):
+    def __init__(self, adata, groups=None, vkey=None, use_time_prior=None, minimum_spanning_tree=None):
+        super().__init__(adata=adata, groups=groups, model='v1.2')
+        if groups is None:
+            groups = 'clusters' if 'clusters' in adata.obs.keys() \
+                else 'louvain' if 'louvain' in adata.obs.keys() else None
         self.groups = groups
+        self.vkey = vkey
+        self.use_time_prior = use_time_prior
+        self.minimum_spanning_tree = minimum_spanning_tree
 
     # overwrite to use flexible vkey
     def compute_transitions(self):
@@ -80,10 +87,20 @@ class PAGA2(PAGA):
         clusters = self._adata.obs[self.groups]
         cats = clusters.cat.categories
         vgraph = (self._adata.uns[vkey] > .1) #* (self._neighbors.distances > 0)
+        if isinstance(self.use_time_prior, str) and self.use_time_prior in self._adata.obs.keys():
+            vpt = self._adata.obs[self.use_time_prior].values
+            rows, cols, vals = [], [], []
+            for i in range(vgraph.shape[0]):
+                indices = vgraph[i].indices
+                idx_bool = vpt[i] < vpt[indices]
+                cols.extend(indices[idx_bool])
+                vals.extend(vgraph[i].data[idx_bool])
+                rows.extend([i] * np.sum(idx_bool))
+            vgraph = vals_to_csr(vals, rows, cols, shape=vgraph.shape)
+
         if 'final_cells' in self._adata.obs.keys() and is_categorical(self._adata.obs['final_cells']):
             final_cells = self._adata.obs['final_cells'].cat.categories
             if len(final_cells) > 0 and isinstance(final_cells[0], str):
-                print(vgraph.shape, clusters.values.isin(final_cells).shape)
                 vgraph[clusters.values.isin(final_cells)] = 0
                 vgraph.eliminate_zeros()
         root = None
@@ -101,7 +118,7 @@ class PAGA2(PAGA):
         transitions = transitions - transitions.T
         transitions_conf = transitions.copy()
         transitions = transitions.tocoo()
-        total_n = self._neighbors.n_neighbors * np.array(vc.sizes()) * 2
+        total_n = self._neighbors.n_neighbors * np.array(vc.sizes())
         for i, j, v in zip(transitions.row, transitions.col, transitions.data):
             reference = np.sqrt(total_n[i] * total_n[j])
             transitions_conf[i, j] = 0 if v < 0 else v / reference
@@ -114,6 +131,18 @@ class PAGA2(PAGA):
             if np.any(idx):
                 indirect = np.clip(T[idx], None, T[i][idx][:, None]).max(0)
                 T[i, T[i] < indirect] = 0
+
+        if self.minimum_spanning_tree:
+            T_tmp = T.copy()
+            T_num = T > 0
+            T_sum = T_num.sum(0)
+            for i in range(len(T_tmp)):
+                if T_sum[i] == 1:
+                    T_tmp[np.where(T_num[:, i])[0][0], i] = 1
+            from scipy.sparse.csgraph import minimum_spanning_tree
+            T_tmp = np.abs(minimum_spanning_tree(-T_tmp).A) > 0
+            T = T_tmp * T
+
         transitions_conf = csr_matrix(T)
         self.transitions_confidence = transitions_conf.T
 
@@ -123,31 +152,21 @@ class PAGA2(PAGA):
             df.pop(root)
         self.threshold = np.nanmin(np.nanmax(df.values / (df.values > 0), axis=0)) - 1e-6
 
+
 def paga(
         adata,
-        vkey='velocity',
         groups=None,
-        model='v1.2',
+        vkey='velocity',
+        use_time_prior=True,
+        minimum_spanning_tree=True,
         copy=False):
     """Mapping out the coarse-grained connectivity structures of complex manifolds [Wolf19]_.
     By quantifying the connectivity of partitions (groups, clusters) of the
     single-cell graph, partition-based graph abstraction (PAGA) generates a much
     simpler abstracted graph (*PAGA graph*) of partitions, in which edge weights
-    represent confidence in the presence of connections. By tresholding this
-    confidence in :func:`~scanpy.pl.paga`, a much simpler representation of the
-    manifold data is obtained, which is nonetheless faithful to the topology of
-    the manifold.
-    The confidence should be interpreted as the ratio of the actual versus the
-    expected value of connetions under the null model of randomly connecting
-    partitions. We do not provide a p-value as this null model does not
-    precisely capture what one would consider "connected" in real data, hence it
-    strongly overestimates the expected value. See an extensive discussion of
-    this in [Wolf19]_.
-    .. note::
-        Note that you can use the result of :func:`~scanpy.pl.paga` in
-        :func:`~scanpy.tl.umap` and :func:`~scanpy.tl.draw_graph` via
-        `init_pos='paga'` to get single-cell embeddings that are typically more
-        faithful to the global topology.
+    represent confidence in the presence of connections.
+    See an extensive disposition of this in [Wolf19]_.
+
     Parameters
     ----------
     adata : :class:`~anndata.AnnData`
@@ -157,52 +176,51 @@ def paga(
         annotation of observations (`adata.obs`).
     vkey: `str` or `None` (default: `None`)
         Key for annotations of observations/cells or variables/genes.
-    model : {'v1.2', 'v1.0'}, optional (default: 'v1.2')
-        The PAGA connectivity model.
+    use_time_prior : `str` or bool, optional (default: True)
+        Obs key for pseudo-time values. If True, 'latent_time' or 'velocity_pseudotime' is used if available.
+    minimum_spanning_tree : bool, optional (default: True)
+        Whether to prune the tree such that a path from A-to-B is removed if another more confident path exists.
     copy : `bool`, optional (default: `False`)
-        Copy `adata` before computation and return a copy. Otherwise, perform
-        computation inplace and return `None`.
+        Copy `adata` before computation and return a copy. Otherwise, perform computation inplace and return `None`.
     Returns
     -------
     **connectivities** : :class:`numpy.ndarray` (adata.uns['connectivities'])
         The full adjacency matrix of the abstracted graph, weights correspond to
         confidence in the connectivities of partitions.
     **connectivities_tree** : :class:`scipy.sparse.csr_matrix` (adata.uns['connectivities_tree'])
-        The adjacency matrix of the tree-like subgraph that best explains
-        the topology.
-    Notes
-    -----
-    Together with a random walk-based distance measure
-    (e.g. :func:`scanpy.tl.dpt`) this generates a partial coordinatization of
-    data useful for exploring and explaining its variation.
-    See Also
-    --------
-    pl.paga
-    pl.paga_path
-    pl.paga_compare
+        The adjacency matrix of the tree-like subgraph that best explains the topology.
+    **transitions_confidence** : :class:`scipy.sparse.csr_matrix` (adata.uns['transitions_confidence'])
+        The adjacency matrix of the abstracted directed graph, weights correspond to
+        confidence in the transitions between partitions.
     """
     if groups is None:
-        groups = 'clusters' if 'clusters' in adata.obs.keys() else 'louvain' if 'louvain' in adata.obs.keys() else 'grey'
+        groups = 'clusters' if 'clusters' in adata.obs.keys() else 'louvain' if 'louvain' in adata.obs.keys() else None
+    if use_time_prior and not isinstance(use_time_prior, str):
+        use_time_prior = 'latent_time' if 'latent_time' in adata.obs.keys() else 'velocity_pseudotime'
+        if use_time_prior not in adata.obs.keys():
+            velocity_pseudotime(adata, vkey=vkey)
     if 'neighbors' not in adata.uns:
         raise ValueError(
             'You need to run `pp.neighbors` first to compute a neighborhood graph.')
     adata = adata.copy() if copy else adata
     strings_to_categoricals(adata)
-    start = logg.info('running PAGA')
-    paga = PAGA2(adata, groups, model=model, vkey=vkey)
-    # only add if not present
+
+    logg.info('running PAGA', r=True)
+    paga = PAGA_tree(adata, groups, vkey=vkey, use_time_prior=use_time_prior, minimum_spanning_tree=minimum_spanning_tree)
+
     if 'paga' not in adata.uns:
         adata.uns['paga'] = {}
+
     paga.compute_connectivities()
     adata.uns['paga']['connectivities'] = paga.connectivities
     adata.uns['paga']['connectivities_tree'] = paga.connectivities_tree
-    # adata.uns['paga']['expected_n_edges_random'] = paga.expected_n_edges_random
     adata.uns[groups + '_sizes'] = np.array(paga.ns)
+
     paga.compute_transitions()
     adata.uns['paga']['transitions_confidence'] = paga.transitions_confidence
     adata.uns['paga']['threshold'] = paga.threshold
-    # adata.uns['paga']['transitions_ttest'] = paga.transitions_ttest
     adata.uns['paga']['groups'] = groups
+
     logg.info('    finished', time=True, end=' ' if settings.verbosity > 2 else '\n')
     logg.hint('added\n' +
               "    'paga/transitions_confidence', connectivities adjacency (adata.uns)\n"
