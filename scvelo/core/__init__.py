6741209d544a58d9ab929674b8b78390b89cc76a
Philipp Weiler <weiler.philipp@gmail.com>
2021-02-01 15:36:39 +0100

    Make functions from `_arithmetic.py` importable

--- scvelo/core/__init__.py
+++ scvelo/core/__init__.py
@@ -1,9 +1,14 @@
 from ._anndata import get_modality, make_dense, make_sparse, set_modality
+from ._arithmetic import clipped_log, invert, prod_sum, sum
 
 
 __all__ = [
+    "clipped_log",
     "get_modality",
+    "invert",
     "make_dense",
     "make_sparse",
+    "prod_sum",
     "set_modality",
+    "sum",
 ]
