1c164d2551c0edacf7bdd56222e4366cc0984d49
Philipp Weiler <weiler.philipp@gmail.com>
2021-02-01 15:53:42 +0100

    Make `LinearRegression` importable

--- scvelo/core/__init__.py
+++ scvelo/core/__init__.py
@@ -1,11 +1,13 @@
 from ._anndata import get_modality, make_dense, make_sparse, set_modality
 from ._arithmetic import clipped_log, invert, prod_sum, sum
+from ._linear_models import LinearRegression
 
 
 __all__ = [
     "clipped_log",
     "get_modality",
     "invert",
+    "LinearRegression",
     "make_dense",
     "make_sparse",
     "prod_sum",
