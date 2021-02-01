2231d7b6038dd7c088a8252f2c93dd96833fc705
Philipp Weiler <weiler.philipp@gmail.com>
2021-02-01 15:54:54 +0100

    Make `l2_norm` importable

--- scvelo/core/__init__.py
+++ scvelo/core/__init__.py
@@ -1,12 +1,14 @@
 from ._anndata import get_modality, make_dense, make_sparse, set_modality
 from ._arithmetic import clipped_log, invert, prod_sum, sum
 from ._linear_models import LinearRegression
+from ._metrics import l2_norm
 
 
 __all__ = [
     "clipped_log",
     "get_modality",
     "invert",
+    "l2_norm",
     "LinearRegression",
     "make_dense",
     "make_sparse",
