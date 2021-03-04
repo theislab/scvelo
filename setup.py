b989269d68a7ff7330a058be7f4087e32bf61106
Philipp Weiler <weiler.philipp@gmail.com>
2021-03-04 13:42:56 +0100

    Bump pre-commit version to 2.9.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     extras_require=dict(
         louvain=["python-igraph", "louvain"],
         hnswlib=["pybind11", "hnswlib"],
-        dev=["black==20.8b1", "flake8==3.8.4", "hypothesis", "pre-commit==2.5.1"],
+        dev=["black==20.8b1", "flake8==3.8.4", "hypothesis", "pre-commit>=2.9.0"],
         docs=[r for r in Path("docs/requirements.txt").read_text("utf-8").splitlines()],
     ),
     packages=find_packages(),
