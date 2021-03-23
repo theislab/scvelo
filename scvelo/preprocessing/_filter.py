6f327c5baeab1dfc72562ae6bdfd4d041f936311
Philipp Weiler <weiler.philipp@gmail.com>
2021-03-23 15:59:09 +0100

    Add GeneCountFilter
    The class is used to filter out variables based on their counrs across
    all observations.

--- scvelo/preprocessing/_filter.py
+++ scvelo/preprocessing/_filter.py
@@ -13,7 +13,7 @@ from ._preprocessing_base import CountFilterBase
 
 
 def _log_progress(
-    kind: Literal["cell_count"],
+    kind: Literal["cell_count", "gene_count"],
     filter_mask: ndarray,
     modality: str,
     lower_bound: float,
@@ -41,6 +41,9 @@ def _log_progress(
     if kind == "cell_count":
         connecting_phrase = "detected in"
         filter_base = "cells"
+    elif kind == "gene_count":
+        connecting_phrase = "with"
+        filter_base = "counts"
 
     if n_filtered_genes > 0:
         logg.info(f"Filtered out {n_filtered_genes} genes ", end=" ")
@@ -167,6 +170,114 @@ class CellCountFilter(CountFilterBase):
 
                 _log_progress(
                     kind="cell_count",
+                    filter_mask=filter_mask,
+                    modality=modality,
+                    lower_bound=self.lower_bound,
+                    upper_bound=self.upper_bound,
+                )
+
+        return adata if not self.inplace else None
+
+
+class GeneCountFilter(CountFilterBase):
+    def __init__(
+        self,
+        inplace: bool = True,
+        enforce: bool = False,
+        min_counts: Optional[int] = None,
+        max_counts: Optional[int] = None,
+        shared_counts: bool = False,
+    ):
+        """Class for filtering variables based on counts across all observations.
+
+        Arguments
+        ---------
+        inplace
+            Boolean flag to indicate whether operations on annotaded data should be
+            performed inplace or not. Otherwise, it is ignored.
+        enforce
+            Boolean flag to enforce filtering modalities.
+        min_counts
+            Minimum number of counts of a variable to not be filtered out.
+        max_counts
+            Maximum number of counts of a variable to not be filtered out.
+        shared_counts
+            Boolearn flag to filter by shared counts.
+        """
+
+        super().__init__(
+            inplace=inplace,
+            enforce=enforce,
+            lower_bound=min_counts,
+            upper_bound=max_counts,
+            shared_counts=shared_counts,
+        )
+
+    def _transform(
+        self,
+        adata: AnnData,
+        modalities: Set,
+        vars_to_keep: Optional[Dict[str, List[int]]] = None,
+    ) -> Optional[AnnData]:
+        """Filter out genes in modalities.
+
+        Arguments
+        ---------
+        adata
+            Annotated data matrix of shape `n_obs` × `n_vars`.
+        modalities
+            Set of modalities to filter.
+        vars_to_keep
+            Dictionary of variables to keep for a modality. If `None`, no variables are
+            excluded from filtering.
+
+        Returns
+        -------
+        Optional[AnnData]
+            Returns annotated data if `self.inplace` is set to `False`, `None`
+            otherwise.
+        """
+
+        if vars_to_keep is None:
+            vars_to_keep = {}
+
+        if self.shared_counts:
+            shared_counts = self._sum_shared_counts(adata=adata, modalities=modalities)
+
+            filter_mask = self.get_filter_mask(
+                adata=adata,
+                modality=shared_counts,
+                vars_to_keep=list(set().union(*vars_to_keep.values())),
+            )
+
+            # Filtering by shared counts requires the count matrices of the modalities
+            # to be of the same size. As the filter mask is the same across all
+            # modalities, it suffices to filter one modality, which automatically
+            # filters all.
+            self._subset_var(
+                adata=adata, modality=modalities.pop(), filter_mask=filter_mask
+            )
+
+            _log_progress(
+                kind="gene_count",
+                filter_mask=filter_mask,
+                modality="shared",
+                lower_bound=self.lower_bound,
+                upper_bound=self.upper_bound,
+            )
+        else:
+            for modality in modalities:
+                filter_mask = self.get_filter_mask(
+                    adata=adata,
+                    modality=get_modality(adata=adata, modality=modality),
+                    vars_to_keep=vars_to_keep.get(modality, []),
+                )
+                self._subset_var(
+                    adata=adata, modality=modality, filter_mask=filter_mask
+                )
+
+                _log_progress(
+                    kind="gene_count",
                     filter_mask=filter_mask,
                     modality=modality,
                     lower_bound=self.lower_bound,
