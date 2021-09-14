c30c335d6c8ac2545f72927bed71862d0747685f
Philipp Weiler <weiler.philipp@gmail.com>
2021-09-14 15:59:03 +0200

    Add `confest.py`
    Add file to declare fixture for entire testing directory.

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+import scanpy as sc
+from anndata import AnnData
+
+# TODO: Make datasets smaller (less variables)
+_dentategyrus_50obs = sc.read("tests/_data/dentategyrus_50obs.h5ad")
+_dentategyrus_100obs = sc.read("tests/_data/dentategyrus_100obs.h5ad")
+_pancreas_50obs = sc.read("tests/_data/pancreas_50obs.h5ad")
+_pancreas_100obs = sc.read("tests/_data/pancreas_100obs.h5ad")
+
+
+@pytest.fixture
+def dentategyrus_50obs() -> AnnData:
+    return _dentategyrus_50obs.copy()
+
+
+@pytest.fixture
+def dentategyrus_100obs() -> AnnData:
+    return _dentategyrus_100obs.copy()
+
+
+@pytest.fixture
+def pancreas_50obs() -> AnnData:
+    return _pancreas_50obs.copy()
+
+
+@pytest.fixture
+def pancreas_100obs() -> AnnData:
+    return _pancreas_100obs.copy()
