5e631dfe304d68bf2d7c256503294b04a8f2b867
Philipp Weiler <weiler.philipp@gmail.com>
2022-12-09 15:04:38 +0100

    Update `setup.py` [ci skip]
    Remove Python 3.6 and 3.7 from classifiers.

--- setup.py
+++ setup.py
@@ -43,8 +43,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
