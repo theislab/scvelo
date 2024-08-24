3dde679e13b0fe7a482fabbc8ccc80d7c8fc6622
IsaacUtah1379 <122501927+IsaacUtah1379@users.noreply.github.com>
2024-08-24 17:22:35 -0400

    Fix conflict with method of setting the categories of a Pandas Series

--- scvelo/tools/rank_velocity_genes.py
+++ scvelo/tools/rank_velocity_genes.py
@@ -186,9 +186,7 @@ def velocity_clusters(
             vc = vc.cat.rename_categories({cat: f"{new_cat} ({cats_nums[new_cat]})"})
         vdata.obs["louvain"] = vc
     else:
-        vdata.obs["louvain"].cat.categories = np.arange(
-            len(vdata.obs["louvain"].cat.categories)
-        )
+        vdata.obs["louvain"].cat.set_categories(np.arange(len(vdata.obs["louvain"].cat.categories)))
     adata.obs[f"{vkey}_clusters"] = vdata.obs["louvain"].copy()
 
     del vdata
