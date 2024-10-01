e2809f9e2ad1c0c8c25c1c8d5cb10c610d624a89
Philipp Weiler <weiler.philipp@gmail.com>
2024-10-01 15:57:41 +0200

    Move logging to dedicated module

---
+++ scvelo/logging/__init__.py
@@ -0,0 +1,21 @@
+from ._logging import (
+    error,
+    hint,
+    info,
+    msg,
+    print_version,
+    ProgressReporter,
+    switch_verbosity,
+    warn,
+)
+
+__all__ = [
+    "error",
+    "hint",
+    "info",
+    "msg",
+    "print_version",
+    "ProgressReporter",
+    "switch_verbosity",
+    "warn",
+]
